e0b2081bae290344ebb8c751934ce70d0fbd8739
agunawan <agunawan@ascentis.com>
2019-12-16 14:52:55 -0800

    Add TrackerLibrary models

---
+++ TrackerLibrary/Models/Prize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary.Models
+{
+    public class Prize
+    {
+        public int PlaceNumber { get; set; }
+        public string PriceName { get; set; }
+        public decimal PrizeAmount { get; set; }
+        public double PrizePercentage { get; set; }
+    }
+}
